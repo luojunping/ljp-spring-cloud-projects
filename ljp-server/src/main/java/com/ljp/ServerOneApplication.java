e2e4e2ffc2efdbe1f1afb9929755fa7f6924a758
罗俊平 <591402399@qq.com>
2020-09-19 09:30:38 +0800

    init project.

---
+++ ljp-server/src/main/java/com/ljp/ServerOneApplication.java
@@ -0,0 +1,13 @@
+package com.ljp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerOneApplication.class, args);
+    }
+
+}
