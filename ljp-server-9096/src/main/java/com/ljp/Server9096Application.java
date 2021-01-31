b05537da0576dfeb1672aec971922b8cd3c8d6fd
罗俊平 <591402399@qq.com>
2021-01-31 16:56:56 +0800

    this is a project about spring, spring boot, spring cloud, spring cloud alibaba.

--- ljp-server-9096/src/main/java/com/ljp/Server9096Application.java
+++ ljp-server-9096/src/main/java/com/ljp/Server9096Application.java
@@ -3,8 +3,10 @@ package com.ljp;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Server9096Application {
 
     public static void main(String[] args) {
